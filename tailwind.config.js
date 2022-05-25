fa782250320ab799a0ae77c21b3688f1f764a513
jakir10 <jakir.cse.bubt@gmail.com>
2022-05-25 13:57:49 +0600

    initial setup done and navbar section added

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#057AFF",
+          secondary: "#463AA1",
+          accent: "#C149AD",
+          neutral: "#021431",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
